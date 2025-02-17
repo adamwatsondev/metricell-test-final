48ad025d9772cb659f0cd65eb647b5bb995307b4
awatson7777 <awatson7777@gmail.com>
2025-02-17 17:52:39 +0000

    added remove functionality, added radixUI, sonner and lucide

--- InterviewTest.Server/Controllers/EmployeesController.cs
+++ InterviewTest.Server/Controllers/EmployeesController.cs
@@ -5,35 +5,51 @@ using Microsoft.Data.Sqlite;
 namespace InterviewTest.Server.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class EmployeesController : ControllerBase
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    [HttpGet]
+    public List<Employee> Get()
     {
-        [HttpGet]
-        public List<Employee> Get()
+        var employees = new List<Employee>();
+
+        var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+        using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
         {
-            var employees = new List<Employee>();
+            connection.Open();
 
-            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
-            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            var queryCmd = connection.CreateCommand();
+            queryCmd.CommandText = @"SELECT ID, Name, Value FROM Employees";
+            using (var reader = queryCmd.ExecuteReader())
             {
-                connection.Open();
-
-                var queryCmd = connection.CreateCommand();
-                queryCmd.CommandText = @"SELECT Name, Value FROM Employees";
-                using (var reader = queryCmd.ExecuteReader())
+                while (reader.Read())
                 {
-                    while (reader.Read())
+                    employees.Add(new Employee
                     {
-                        employees.Add(new Employee
-                        {
-                            Name = reader.GetString(0),
-                            Value = reader.GetInt32(1)
-                        });
-                    }
+                        ID = reader.GetInt32(0),  // Make sure you get the ID
+                        Name = reader.GetString(1),
+                        Value = reader.GetInt32(2)
+                    });
                 }
             }
+        }
 
-            return employees;
+        return employees;
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        using (var connection = new SqliteConnection("Data Source=./SqliteDB.db"))
+        {
+            connection.Open();
+            var deleteCmd = connection.CreateCommand();
+            deleteCmd.CommandText = "DELETE FROM Employees WHERE ID = @id";
+            deleteCmd.Parameters.AddWithValue("@id", id);
+            int affectedRows = deleteCmd.ExecuteNonQuery();
+
+            return affectedRows > 0 ? Ok() : NotFound();
         }
     }
+}
 }
