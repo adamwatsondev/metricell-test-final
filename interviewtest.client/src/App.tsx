5b009ac1d413fd10b971f4c6513ce45ff3b1de39
awatson7777 <awatson7777@gmail.com>
2025-02-17 19:44:50 +0000

    add functionality working, edit in progress

--- interviewtest.client/src/App.tsx
+++ interviewtest.client/src/App.tsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
-import { Button, Table, Theme } from "@radix-ui/themes";
+import {
+  Button,
+  Dialog,
+  Flex,
+  Table,
+  Text,
+  TextField,
+  Theme,
+} from "@radix-ui/themes";
 import { Toaster, toast } from "sonner";
 
 interface Employee {
@@ -12,7 +20,8 @@ interface Employee {
 
 function App() {
   const [employees, setEmployees] = useState<Employee[]>([]);
-  //   const [newEmployee, setNewEmployee] = useState({ name: "", value: 0 });
+  const [newEmployee, setNewEmployee] = useState({ name: "", value: 0 });
+  const [editingEmployee, setEditingEmployee] = useState<Employee | null>(null);
 
   useEffect(() => {
     fetchEmployees();
@@ -22,6 +31,31 @@ function App() {
     const response = await fetch("api/employees");
     const data = await response.json();
     setEmployees(data);
+  };
+
+  const addEmployee = async () => {
+    try {
+      const response = await fetch("api/employees", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newEmployee),
+      });
+
+      if (response.ok) {
+        fetchEmployees();
+        setNewEmployee({ name: "", value: 0 });
+        toast.success("Employee added successfully.");
+      } else {
+        const errorMessage = await response.text();
+        console.error("Failed to add employee:", errorMessage);
+        toast.error("Failed to add employee");
+      }
+    } catch (error) {
+      console.error("Error adding employee:", error);
+      toast.error("Failed to add employee");
+    }
   };
 
   const removeEmployee = async (id: number) => {
@@ -41,7 +75,35 @@ function App() {
       toast.success("Removed Successfully");
     } catch (error) {
       console.error("Error deleting employee:", error);
+      toast.error("Failed to remove employee");
       fetchEmployees();
+    }
+  };
+
+  const updateEmployee = async () => {
+    if (editingEmployee) {
+      try {
+        const response = await fetch(`api/employees/${editingEmployee.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(editingEmployee),
+        });
+
+        if (response.ok) {
+          fetchEmployees();
+          toast.success("Employee updated successfully.");
+          setEditingEmployee(null);
+        } else {
+          const errorMessage = await response.text();
+          console.error("Failed to update employee:", errorMessage);
+          toast.error("Failed to update employee");
+        }
+      } catch (error) {
+        console.error("Error updating employee:", error);
+        toast.error("Failed to update employee");
+      }
     }
   };
 
@@ -68,30 +130,109 @@ function App() {
               <Table.Cell>{employee.name}</Table.Cell>
               <Table.Cell>{employee.value}</Table.Cell>
               <Table.Cell className="flex justify-end gap-2 items-center">
-                <Button
-                  color="gray"
-                  variant="outline"
-                  highContrast
-                  onClick={() => removeEmployee(employee.id)}
+                <Dialog.Root
+                  onOpenChange={(open) => open || setEditingEmployee(null)}
                 >
-                  <img
-                    src="/icons/edit.svg"
-                    className="text-white size-4"
-                    alt="edit"
-                  />
-                </Button>
-                <Button
-                  color="red"
-                  variant="classic"
-                  highContrast
-                  onClick={() => removeEmployee(employee.id)}
-                >
-                  <img
-                    src="/icons/delete.svg"
-                    className="text-white size-6"
-                    alt="delete"
-                  />
-                </Button>
+                  <Dialog.Trigger>
+                    <Button color="blue">Edit</Button>
+                  </Dialog.Trigger>
+
+                  <Dialog.Content maxWidth="450px">
+                    <Dialog.Title>Edit Entry</Dialog.Title>
+
+                    <Flex direction="column" gap="3">
+                      <label>
+                        <Text as="div" size="2" mb="1" weight="bold">
+                          Name
+                        </Text>
+                        <TextField.Root
+                          value={editingEmployee?.name || ""}
+                          onChange={(e) =>
+                            setEditingEmployee({
+                              ...editingEmployee!,
+                              name: e.target.value,
+                            })
+                          }
+                        />
+                      </label>
+                      <label>
+                        <Text as="div" size="2" mb="1" weight="bold">
+                          Value
+                        </Text>
+                        <TextField.Root
+                          value={editingEmployee?.value || ""}
+                          type="number"
+                          onChange={(e) =>
+                            setEditingEmployee({
+                              ...editingEmployee!,
+                              value: e.target.value
+                                ? parseInt(e.target.value)
+                                : 0,
+                            })
+                          }
+                        />
+                      </label>
+                    </Flex>
+
+                    <Flex gap="3" mt="4" justify="end">
+                      <Dialog.Close>
+                        <Button variant="soft" color="gray">
+                          Cancel
+                        </Button>
+                      </Dialog.Close>
+                      <Dialog.Close>
+                        <Button
+                          onClick={updateEmployee}
+                          variant="solid"
+                          color="green"
+                        >
+                          Update
+                        </Button>
+                      </Dialog.Close>
+                    </Flex>
+                  </Dialog.Content>
+                </Dialog.Root>
+
+                <Dialog.Root>
+                  <Dialog.Trigger>
+                    <Button color="red">
+                      <img
+                        src="/icons/delete.svg"
+                        className="text-white size-6"
+                        alt="delete"
+                      />
+                    </Button>
+                  </Dialog.Trigger>
+
+                  <Dialog.Content maxWidth="450px">
+                    <Dialog.Title>Delete Confirmation</Dialog.Title>
+
+                    <Flex direction="column" gap="3">
+                      <label>
+                        <Text as="div" size="2" mb="1" weight="regular">
+                          Are you sure you want to delete this entry?
+                        </Text>
+                      </label>
+                    </Flex>
+
+                    <Flex gap="3" mt="4" justify="end">
+                      <Dialog.Close>
+                        <Button variant="soft" color="gray">
+                          Cancel
+                        </Button>
+                      </Dialog.Close>
+                      <Dialog.Close>
+                        <Button
+                          onClick={() => removeEmployee(employee.id)}
+                          variant="solid"
+                          color="red"
+                        >
+                          Delete
+                        </Button>
+                      </Dialog.Close>
+                    </Flex>
+                  </Dialog.Content>
+                </Dialog.Root>
               </Table.Cell>
             </Table.Row>
           ))}
@@ -111,10 +252,69 @@ function App() {
             <span className="text-2xl font-bold">
               Employees: {employees.length}
             </span>
-            <div className="text-2xl h-12 bg-green-500 rounded-md items-center p-4 flex gap-2 font-bold">
-              <img src="/icons/plus.svg" className="size-6" alt="add" />
-              <span className="text-white">Add</span>
-            </div>
+            <Dialog.Root>
+              <Dialog.Trigger>
+                <Button color="green">
+                  <img
+                    src="/icons/plus.svg"
+                    className="text-white size-6"
+                    alt="add"
+                  />
+                </Button>
+              </Dialog.Trigger>
+
+              <Dialog.Content maxWidth="450px">
+                <Dialog.Title>Add Entry</Dialog.Title>
+                <Dialog.Description size="2" mb="4">
+                  Add an entry to the database.
+                </Dialog.Description>
+
+                <Flex direction="column" gap="3">
+                  <label>
+                    <Text as="div" size="2" mb="1" weight="bold">
+                      Name
+                    </Text>
+                    <TextField.Root
+                      placeholder="Enter a first name"
+                      value={newEmployee.name}
+                      onChange={(e) =>
+                        setNewEmployee({ ...newEmployee, name: e.target.value })
+                      }
+                    />
+                  </label>
+                  <label>
+                    <Text as="div" size="2" mb="1" weight="bold">
+                      Value
+                    </Text>
+                    <TextField.Root
+                      placeholder="Enter a value"
+                      type="number"
+                      value={newEmployee.value || ""}
+                      onChange={(e) => {
+                        const newValue = e.target.value;
+                        setNewEmployee({
+                          ...newEmployee,
+                          value: newValue === "" ? 0 : parseInt(newValue) || 0,
+                        });
+                      }}
+                    />
+                  </label>
+                </Flex>
+
+                <Flex gap="3" mt="4" justify="end">
+                  <Dialog.Close>
+                    <Button variant="soft" color="gray">
+                      Cancel
+                    </Button>
+                  </Dialog.Close>
+                  <Dialog.Close>
+                    <Button onClick={addEmployee} variant="solid" color="green">
+                      Add
+                    </Button>
+                  </Dialog.Close>
+                </Flex>
+              </Dialog.Content>
+            </Dialog.Root>
           </div>
           <div className="flex flex-col gap-4">
             <EmployeeTable
